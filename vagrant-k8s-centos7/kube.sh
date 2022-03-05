9145996e5828af2f3b3e1e92db864adbe620721c
hul0810 <67278262+hul0810@users.noreply.github.com>
2022-03-05 22:01:26 +0900

    vagrant로 centos7에 k8s 구축 자동화

---
+++ vagrant-k8s-centos7/kube.sh
@@ -0,0 +1,50 @@
+#/bin/bash
+
+# docker install
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl enable docker
+sudo systemctl start docker
+
+cat << EOF > /etc/docker/daemon.json
+{"exec-opts": ["native.cgroupdriver=systemd"]}
+EOF
+
+sudo systemctl restart docker
+
+# get ready to install kubernetes(system configuration)
+sudo swapoff -a
+sudo sed -i '/ swap / s/^/#/' /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+
+# kubernetes install
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+sudo yum install -y kubelet-$k8s_ver kubeadm-$k8s_ver kubectl-$k8s_ver --disableexcludes=kubernetes
+
+sudo systemctl enable --now kubelet
