f9b48fff6520f161543f20f37ffd4fb78f17aca9
hul0810 <67278262+hul0810@users.noreply.github.com>
2022-03-05 21:59:02 +0900

    Delete vagrant-k8s-centos7 directory

--- vagrant-k8s-centos7/centos7_ssh_password_access_allow.sh
+++
@@ -1,5 +0,0 @@
-#/bin/bash
-now=$(date +%m_%d_%Y)
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config_$now.bak
-sudo sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
-systemctl restart sshd
